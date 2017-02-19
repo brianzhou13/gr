7ee3f8931bb69bdbe2c0605c4ebc3f055ac23319
Brian Zhou <bzhou4@gmail.com>
2017-02-19 15:29:03 -0800

    input form added

--- src/client/components/Search.jsx
+++ src/client/components/Search.jsx
@@ -5,12 +5,35 @@ class Search extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      value: 'city'
+    };
+
+    this.updateText = this.updateText.bind(this);
+  }
+
+  updateText(el) {
+    this.setState({
+      value: el.target.value
+    });
   }
 
   render() {
     return (
       <div>
-        Results
+        <div className="form-inline row">
+          <label 
+            for="example-text-input" 
+            className="col-2 col-form-label">Text</label>
+          <div className="col-10">
+            <input 
+              className="form-control" 
+              type="text" 
+              value={this.state.value}
+              onChange={this.updateText}
+              id="example-text-input"/>
+          </div>
+        </div>
       </div>
     )
   }
